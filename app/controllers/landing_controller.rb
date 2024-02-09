3c9eed1d88c6e48196d37bbfc5faf80c9b2dda78
LilyTTT <yushantang4@gmail.com>
2024-02-08 18:29:48 -0600

    Resolved Logo and stylesheet path issues

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,7 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def signup
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   root "landing#index"
 
   get 'landing/index'
+  get 'landing/signup'
+
+  # resources :landing do
+  #   member do
+  #     get :delete
+  #   end
+  # end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
