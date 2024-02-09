18c3e3f88ebdfa0cb678e35a36579f607320260d
LilyTTT <yushantang4@gmail.com>
2024-02-08 19:24:05 -0600

    Renamed index.css to landing.css to be consistent with the controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w( jimmy_logo.jpg )
-Rails.application.config.assets.precompile += %w( index.css )
+Rails.application.config.assets.precompile += %w( landing.css )
 
 
 # Add additional assets to the asset load path.
