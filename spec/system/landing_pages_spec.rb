9f39895f5a15dd8190b48bb6e004eb13649a4b35
FF7Pneuma <kawanardalan@hotmail.com>
2024-02-12 07:09:57 -0600

    Added more tests for the default landing page

--- spec/system/landing_pages_spec.rb
+++ spec/system/landing_pages_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe "LandingPages", type: :system do
 
     expect(page).to have_content('Jimmy')
   end
+
+  it 'check for copyright footer' do
+    visit '/landing/index'
+
+    expect(page).to have_content('Teamup is a 501(c)(3) nonprofit organization.')
+  end
+
+  it 'navigating from landing page to log in / sign up' do
+    visit '/landing/index'
+    click_on 'Log in / Sign up'
+
+    expect(page).to have_content('Seems like you\'re new to Jimmy...')
+  end
 end
