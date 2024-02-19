41fbb8d24c39135a95b5842ff3943f7abf67163e
LilyTTT <yushantang4@gmail.com>
2024-02-19 16:12:38 -0600

    Create controller for profile customization

---
+++ app/controllers/customization_controller.rb
@@ -0,0 +1,16 @@
+class CustomizationController < ApplicationController
+  def index
+  end
+
+  def personalInfo
+  end
+
+  def personalPref
+  end
+
+  def workoutPref
+  end
+
+  def socialMedia
+  end
+end
