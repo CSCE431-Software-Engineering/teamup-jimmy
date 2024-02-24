020b98d2f71d867a81322e30bdfbb4c7893ab706
LilyTTT <yushantang4@gmail.com>
2024-02-24 16:27:03 -0600

    -ACTION REQUIRED- db rollback when adding activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesController < ApplicationController
 
     @activity_ids = ActivityPreference.where(student_email: @current_student.email).pluck(:activity_id)
     @exp_levels = ActivityPreference.where(student_email: @current_student.email).pluck(:experience_level)
-    @activities = Activity.where(id: @activity_ids).pluck(:activity_name)
+    @current_activities = Activity.where(id: @activity_ids).pluck(:activity_name)
   end
 
   def new
@@ -15,10 +15,22 @@ class ActivitiesController < ApplicationController
   end
 
   def edit
+    @current_student = Student.find_by(email: session[:student_id])
+
     @activity = Activity.find(params[:id])
     @levels = ['Beginner', 'Novice', 'Intermediate', 'Advanced', 'Expert']
+
+    @new_pref = ActivityPreference.new(activity_id: @activity.id, student_email: @current_student.email)
+
+    if @new_pref.save
+      redirect_to activities_path
+    else
+      render('edit')
+    end
+
   end
 
   def update
   end
+
 end
