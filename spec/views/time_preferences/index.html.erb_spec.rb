bc600f1e1256fbcb40a40f3ea7013d2af3ed936e
LilyTTT <yushantang4@gmail.com>
2024-02-25 16:27:26 -0600

    Fix table formatting code

--- spec/views/time_preferences/index.html.erb_spec.rb
+++ spec/views/time_preferences/index.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "time/index.html.erb", type: :view do
+RSpec.describe "time_preferences/index.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
 end
