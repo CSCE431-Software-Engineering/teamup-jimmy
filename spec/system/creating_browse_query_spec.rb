77f8e65b2e8e79f805f84665b11e4fed5a677361
kieranbeirne <kieranbeirne01@gmail.com>
2024-03-07 12:49:30 -0600

    seeded database for browse test

--- spec/system/creating_browse_query_spec.rb
+++ spec/system/creating_browse_query_spec.rb
@@ -1,16 +1,40 @@
 RSpec.feature "CreateBrowseQuery", type: :feature do
     before do
-      # Create student profile
-      visit '/students/basic'
-      fill_in 'student_name', with: 'Cloud Strife'
-      fill_in 'student_email', with: 'cloudstrife@tamu.edu'
-      select 'Male', from: 'student_gender'
-      fill_in 'student_birthday', with: '1986-08-11'
-      click_on 'Create Profile'
-  
-      expect(page).to have_content('You\'re signed in!')
-      visit '/pages/browse'
-    end
+        # Directly create records in the test database
+        Student.create!(
+          name: 'kb testing',
+          email: 'kb@example.com',
+          gender: 'Other', # Set the correct gender if needed
+          birthday: '2000-01-01', # Adjust the date as necessary
+          is_private: 'false'
+        )
+        Student.create!(
+          name: 'Firsy Lasy',
+          email: 'firsyl@example.com',
+          gender: 'Male',
+          birthday: '2000-01-01',
+          is_private: 'false'
+
+        )
+        Student.create!(
+          name: 'Lily Tang',
+          email: 'lilyt@example.com',
+          gender: 'Female',
+          birthday: '2000-01-01',
+          is_private: 'false'
+        )
+    
+        # Create a user to perform the search
+        visit '/students/basic'
+        fill_in 'student_name', with: 'Cloud Strife'
+        fill_in 'student_email', with: 'cloudstrife@tamu.edu'
+        select 'Male', from: 'student_gender'
+        fill_in 'student_birthday', with: '1986-08-11'
+        click_on 'Create Profile'
+    
+        expect(page).to have_content('You\'re signed in!')
+        visit '/pages/browse'
+      end
 
     scenario 'Sunny: Can search for users with no gender filter' do
         fill_in 'search-input', with: 'K'
