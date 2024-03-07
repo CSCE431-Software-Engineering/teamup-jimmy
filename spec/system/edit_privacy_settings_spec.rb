bb32dd3268fd7d879e7fb1efffbe5a3ee170483c
kieranbeirne <kieranbeirne01@gmail.com>
2024-03-07 12:17:41 -0600

    added integrity cases for setting account provate

---
+++ spec/system/edit_privacy_settings_spec.rb
@@ -0,0 +1,39 @@
+# spec/features/edit_privacy_settings_spec.rb
+require 'rails_helper'
+
+RSpec.feature "EditPrivacySettings", type: :feature do
+  before do
+    # Create student profile
+    visit '/students/basic'
+    fill_in 'student_name', with: 'Cloud Strife'
+    fill_in 'student_email', with: 'cloudstrife@tamu.edu'
+    select 'Male', from: 'student_gender'
+    fill_in 'student_birthday', with: '1986-08-11'
+    click_on 'Create Profile'
+
+    expect(page).to have_content('You\'re signed in!')
+    visit '/students/edit_is_private'
+  end
+
+  scenario 'Clicking back button returns to the previous page' do
+    click_link 'Back'
+    expect(page).to have_current_path(students_personal_info_path)
+  end
+
+  scenario 'Successfully sets account to private' do
+    choose 'is_private_true' # This will look for an element with id 'is_private_true'
+    click_button 'Save'
+    expect(page).to have_content('Your account was successfully updated.')
+  end
+
+  scenario 'Successfully sets account to public' do
+    choose 'is_private_false' # This will look for an element with id 'is_private_false'
+    click_button 'Save'
+    expect(page).to have_content('Your account was successfully updated.')
+  end
+
+  scenario 'Form submission successful when no option is selected' do
+    click_button 'Save'
+    expect(page).to have_content('Your account was successfully updated.') 
+  end
+end
