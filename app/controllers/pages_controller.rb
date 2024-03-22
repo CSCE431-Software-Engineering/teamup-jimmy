d1d1def99b1b567d11045917e6723ca52a9b07df
LilyTTT <yushantang4@gmail.com>
2024-03-22 13:04:30 -0500

    Add show matched profile cards on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,11 @@ class PagesController < ApplicationController
   def home
     # Fetch the current student
     @current_student = Student.find_by(email: session[:student_id])
-    @matches = Match.where("student1_email = ?", @current_student.email)
+
+    @existing_matches_A = Match.where(student1_email: @current_student, relationship_enum: 3)
+    @existing_matches_B = Match.where(student2_email: @current_student, relationship_enum: 3)
+    
+    @matched_emails = Student.where(email: @existing_matches_A&.pluck(:student2_email)).or(Student.where(email: @existing_matches_B&.pluck(:student1_email)))  
   end
 
   def match; end
