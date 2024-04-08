d9ce21985d50a3db0fceaf42df772df80328593b
AllieSaxton03 <47509927+AllieSaxton03@users.noreply.github.com>
2024-04-08 13:57:49 -0500

    Updated gym and activity catalog, revamped navigation bars for consistency, added dynamic searching to activity list.  (#20)
    * attempting to fix copy to clipboard on hosted site
    
    * fixed copy phone number js
    
    * Update application.css
    
    * removed extra nav css
    
    * attempting to fix matching pass error
    
    * fixing bullet css on mobile
    
    * fixing bullet css on mobile
    
    * fixed copy phone number js
    
    * fixing bullet css on mobile
    
    * pelase work
    
    * fixing bullet css on mobile
    
    * Restore navbar.css
    
    * Update font
    
    * css pls
    
    * fixed navbar css becoming invisible again
    
    * fixed spontaneous signout error bug
    
    * temporarily removing restriction on tamu.edu domains
    
    * updated the list of activities and gyms as per dr kebos request
    
    * updated flash alerts to include error message and converted biography to a larger text area to improve usability
    
    * fixed small css detail in header
    
    * updating the nav bar for inner pages
    
    * revamped all the headers using responsive css and partials
    
    * added dynamic searching to activity list
    
    * changed the activity to a 'dropdown'
    
    ---------
    
    Co-authored-by: Kieran Beirne <98914611+kieranbeirne@users.noreply.github.com>
    Co-authored-by: LilyTTT <yushantang4@gmail.com>

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,16 +13,16 @@ class Student < ApplicationRecord
   validates :gender, presence: true, inclusion: { in: %w[Male Female Other] }
   validates :birthday, presence: true
   validates :major, presence: false
-  validates :major, format: { with: /\A[a-zA-Z\s]+\z/, message: "can only contain alphabetic values and spaces" }, allow_blank: true
+  validates :major, format: { with: /\A[a-zA-Z\s]+\z/, message: "*Major can only contain alphabetic values and spaces." }, allow_blank: true
   validates :is_private, presence: false
-  validates :biography, presence: false, length: { maximum: 144 }
+  validates :biography, presence: false, length: { maximum: 144, message: "*Biography must be less than 144 characters." }
   validates :age_start_pref, presence: true
   validates :age_end_pref, presence: true, numericality: { greater_than_or_equal_to: :age_start_pref }
-  validates :x_url, format: { with: /\A(?:(?:http|https):\/\/)?(?:www\.)?x\.com\/.+\z/, message: "must be a valid X URL" }, allow_blank: true
-  validates :instagram_url, format: { with: /\A(?:(?:http|https):\/\/)?(?:www\.)?instagram\.com\/.+\z/, message: "must be a valid Instagram URL" }, allow_blank: true
+  validates :x_url, format: { with: /\A(?:(?:http|https):\/\/)?(?:www\.)?x\.com\/.+\z/, message: "*Input must be a valid X URL." }, allow_blank: true
+  validates :instagram_url, format: { with: /\A(?:(?:http|https):\/\/)?(?:www\.)?instagram\.com\/.+\z/, message: "*Input must be a valid Instagram URL." }, allow_blank: true
 
-  validates :phone_number, format: { with: /\A\d{10}\z/, message: "must be exactly 10 digits long" }, allow_blank: true
-  validates :grad_year, format: { with: /\A\d{4}\z/, message: "must be a four-digit year" }, allow_blank: true
+  validates :phone_number, format: { with: /\A\d{10}\z/, message: "*Phone number must be exactly 10 digits long." }, allow_blank: true
+  validates :grad_year, format: { with: /\A\d{4}\z/, message: "*Graduation year must be a four-digit number." }, allow_blank: true
 
 
   validates_inclusion_of :gender_pref_female, in: [true, false]
